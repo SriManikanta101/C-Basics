6867dbff50c94ee8b3b3ddf8e68381d68c06b265
SriManikanta101 <92290699+SriManikanta101@users.noreply.github.com>
2021-10-20 12:29:58 +0530

    Add files via upload

---
+++ student details [ declaratin + initialization].c
@@ -0,0 +1,14 @@
+/* C program to print details of a person */
+#include<stdio.h>
+void main()
+{
+	//variable declaration and initalization
+	int age=19;
+	float cgpa=9.2;
+	char initial='p';
+	
+	//output
+	printf("students age is:%i\n",age);
+	printf("students cgpa is:%f\n",cgpa);
+	printf("students initial is:%c",initial);
+}
