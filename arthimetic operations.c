a6808b63339957504fa5669193ac783a21d903a2
SriManikanta101 <92290699+SriManikanta101@users.noreply.github.com>
2021-11-02 11:50:04 +0530

    Add files via upload

---
+++ arthimetic operations.c
@@ -0,0 +1,29 @@
+// All Arthimetic opertions */
+#include<stdio.h>
+void main()
+{
+    int a,b,c,d,e,f,g,h,i,j,k,l,m,n,o;
+	a = 111;
+	b = 222;
+	c = 333;
+	d = 444;
+	e = 555;
+	f = 666;
+	g = 777;
+	h = 888;
+	i = 999;
+	j = 1111;
+		
+		
+	k = a+b;
+	l = c-d;
+	m = e*f;
+	n = g/h;
+	o = i%j;
+	
+	printf("sum of %d and %d is %d\n",a,b,k);
+    printf("difference of %d and %d is %d\n",c,d,l);
+	printf("product of %d and %d is %d\n",e,f,m);
+	printf("quotient of %d and %d is %d\n",g,h,n);
+	printf("remainder of %d and %d is %d\n",i,j,o);
+}
