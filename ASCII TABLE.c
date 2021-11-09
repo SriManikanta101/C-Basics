bbec15c1e88795713daf4c1b4706e52b1710fe82
SriManikanta101 <92290699+SriManikanta101@users.noreply.github.com>
2021-11-09 12:19:00 +0530

    Add files via upload

---
+++ ASCII TABLE.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+void main()
+{
+	int c;
+	
+	printf("Enter a number (65-90 or 97-122):");
+	
+	scanf("%d",&c);
+	
+	printf("%c",c);
+	
+}
---
+++ ascii char.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+void main()
+{
+	char c;
+	
+	printf("Enter a character:");
+	
+	scanf("%c",&c);
+	
+	printf("%d",c);
+	
+}
