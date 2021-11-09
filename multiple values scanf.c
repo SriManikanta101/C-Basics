ccc9c28d7c9aa71e57f98bcf7d764a295ebd1e19
SriManikanta101 <92290699+SriManikanta101@users.noreply.github.com>
2021-11-09 11:51:46 +0530

    Add files via upload

---
+++ multiple values scanf.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+void main()
+{
+	float a,b,c;
+	
+	printf("Enter three numbers : ");
+	
+	scanf("%f%f%f", &a, &b, &c);
+	
+	printf("number1 : %f\n", a);
+	printf("number2 : %f\n", b);
+	printf("number3 : %f\n", c);
+	
+
+}
---
+++ multiplication of 4 numbers.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+void main()
+{
+	int a,b,c,d;
+	
+	
+	printf("Enter four numbers : ");
+	
+	scanf("%d%d%d%d", &a, &b, &c, &d);
+	
+	printf("product of %d,%d,%d and %d is %d",a,b,c,d,a*b*c*d);
+	
+	
+
+}
